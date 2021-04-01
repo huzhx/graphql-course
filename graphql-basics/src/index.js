0e68ca9f1af0bde5eedb00f4607367f7bfe21633
Zhaoxian Hu <huzhx@uci.edu>
2021-04-01 14:28:59 -0700

    extracted the schema in its own file

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -74,68 +74,6 @@ let comments = [
   },
 ];
 
-// Type definition (schema)
-const typeDefs = `
-    type Query {
-        users(query: String): [User!]!
-        me: User!
-        posts(query: String): [Post!]!
-        post: Post!
-        comments: [Comment!]!
-    }
-
-    type Mutation {
-      createUser(data: CreateUserInput!): User!
-      deleteUser(id: ID!): User!
-      createPost(data: CreatePostInput!): Post!
-      createComment(data: createCommentInput!): Comment!
-    }
-
-    input CreateUserInput {
-      name: String! 
-      email: String!
-      age: Int
-    }
-
-    input CreatePostInput {
-      title: String!
-      body: String!
-      published: Boolean!
-      author: ID!
-    }
-
-    input createCommentInput {
-      text: String!
-      author: ID!
-      post: ID!
-    }
-
-    type User {
-        id: ID!
-        name: String!
-        email: String!
-        age: Int
-        posts: [Post!]!
-        comments: [Comment!]!
-    }
-
-    type Post {
-        id: ID!
-        title: String!
-        body: String!
-        published: Boolean!
-        author: User!
-        comments: [Comment!]!
-    }
-
-    type Comment {
-        id: ID!
-        text: String!
-        author: User!
-        post: Post!
-    }
-`;
-
 // Resolvers
 const resolvers = {
   Query: {
@@ -271,7 +209,7 @@ const resolvers = {
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 });
 
