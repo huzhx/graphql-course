2bf0a1db3f6cc93ba31c28db960cd1e20bd9f2ca
Zhaoxian Hu <huzhx@uci.edu>
2021-03-31 23:13:20 -0700

    define a mutation createComment

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -87,6 +87,7 @@ const typeDefs = `
     type Mutation {
       createUser(name: String!, email: String!, age: Int): User!
       createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+      createComment(text: String!, author: ID!, post: ID!): Comment!
     }
 
     type User {
@@ -183,6 +184,28 @@ const resolvers = {
       };
       posts.push(post);
       return post;
+    },
+    createComment(parent, args, ctx, info) {
+      const userExists = users.some((user) => user.id === args.author);
+      if (!userExists) {
+        throw new Error('User not found');
+      }
+      const postExists = posts.some((post) => post.id === args.post);
+      if (!postExists) {
+        throw new Error('Post not found');
+      }
+      const post = posts.find((post) => post.id === args.post);
+      if (!post.published) {
+        throw new Error('Post not published');
+      }
+      const comment = {
+        id: uuidv4(),
+        text: args.text,
+        author: args.author,
+        post: args.post,
+      };
+      comments.push(comment);
+      return comment;
     },
   },
   Post: {
